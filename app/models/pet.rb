acdf8b11236047edd77d44e86ff9768ded3fd0f6
charles-henri poniard <c.poniard@gmail.com>
2020-06-09 16:31:13 +0200

    ok

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,9 @@
 class Pet < ApplicationRecord
-
   SPECIES = ["cat", "dog", "snake", "pig"]
   validates :name, presence: true
   validates :species,  inclusion: { in: SPECIES, message: "try again"}
 
+  def found_days_ago
+    return ( Date.today - found_on ).to_i
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "remove all pets "
+Pet.delete_all
+
+puts "create pets "
+Pet.create!(name: "Lolotte", address: "160 av du général de gaulle", found_on: Date.today - 3, species: "cat")
+Pet.create!(name: "Paupiette", address: "8 cours de la martinique", found_on: Date.today - 44, species: "cat")
+Pet.create!(name: "Edmond", address: "44 rue de la ferme", found_on: Date.today - 10, species: "pig")
+puts "Seed Ok"
